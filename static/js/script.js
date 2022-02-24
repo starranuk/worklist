bf4b682a276e168e81ed765d1a4b4293827934d2
starranuk <starran@live.co.uk>
2022-02-24 14:14:09 +0000

    collapsable menu in jobs started

--- static/js/script.js
+++ static/js/script.js
@@ -4,10 +4,17 @@
 //});
 
 /*
-    vanilla JavaScript for MaterializeCSS initialization
+    JavaScript for Materialize NavBar 
 */
 
 document.addEventListener('DOMContentLoaded', function () {
      let sidenavs = document.querySelectorAll(".sidenav");
      let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
  });
+
+ /*
+    jQuery for Materialize accordion element
+*/
+ $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
