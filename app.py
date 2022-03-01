66b24a28f6aece51d886b3f89cb3f033915b7136
starranuk <starran@live.co.uk>
2022-03-01 11:46:22 +0000

    moved delete button to edit job page and resolved remove method error, should be delete_one

--- app.py
+++ app.py
@@ -147,6 +147,12 @@ def edit_job(job_id):
     return render_template("edit_job.html", job=job, job_type=job_type)
 
 
+@app.route("/delete_job/<job_id>")
+def delete_job(job_id):
+    mongo.db.jobs.delete_one({"_id": ObjectId(job_id)})
+    flash("Job Deleted!")
+    return redirect(url_for("get_jobs"))
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
